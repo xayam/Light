f532cd4ea8e03ad9fce15fb0d8cd33fe5a6949c4
xayam <xayam@yandex.ru>
2024-02-04 19:10:01 +0300

    v0.54

--- src/square/plot.py
+++ src/square/plot.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 from liner import create_raw, encode
 
 
-file_name = "fb536381.txt"
+file_name = "random.txt"
 
 moves = create_raw(file_name)
+count = 0
 while True:
     raw = moves[:]
     moves = encode(moves)
-    plt.plot(raw)
-    plt.show()
+    # plt.plot(raw)
+    # plt.show()
+    count += 1
     if max(moves) == 1 and max(raw) == 1:
         json_string = json.dumps(moves)
         with open(f"{file_name}.moves.json", mode="w") as f:
             f.write(json_string)
-        json_string = json.dumps(raw)
-        with open(f"{file_name}.raw.json", mode="w") as f:
-            f.write(json_string)
         break
+print(count)
