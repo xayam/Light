03ba06e50143f5f16f49444d15714a926513ec88
xayam <xayam@yandex.ru>
2024-02-05 18:26:41 +0300

    v0.56

--- src/square/liner.py
+++ src/square/liner.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-width = 256
+width = 16
 file_name = "fb536381.txt"
 
 
@@ -34,7 +34,7 @@ def create_raw(file_name):
             if buf:
                 moves.append(int.from_bytes(buf, "little"))
             count += 1
-            if count == 256 ** 2:
+            if count == width:
                 break
     return moves
 
@@ -43,31 +43,46 @@ def encode(raw):
     moves = []
     pos = 0
     count = 0
-    how = []
     for char in range(max(raw) + 1):
         progress(f"{char + 1}/{max(raw) + 1}")
         while char in raw[pos:]:
-            # curr = pos + count
             count = 0
             while pos + count < len(raw):
                 if raw[pos + count] == char:
                     break
                 count += 1
             raw = raw[:pos] + raw[pos + count:] + raw[pos:pos + count]
-            pos += 1
             moves.append(count)
-        how.append([char, pos])
+            pos += 1
+
     print("")
     print(len(moves), min(moves), max(moves), sep=":")
     print(len(raw), min(raw), max(raw), sep=":")
-    return how, moves, raw
+    return moves, raw
 
 
-def decode(result):
-    counts = result[0][0]
-    moves = result[0][1]
-    raw = result[0][2]
-    print(moves)
+def decode(raw, moves):
+    output = [-1 for _ in range(len(moves))]
+    pos = 0
+    for index in range(len(raw)):
+        count = raw[index] + 1
+        while count != 0:
+            # print(count)
+            if output[pos] < 0:
+                count -= 1
+            pos += 1
+            if pos >= len(raw):
+                pos = 0
+        if pos == 0:
+            pos = len(raw) - 1
+        else:
+            pos -= 1
+        output[pos] = moves[index]
+    # print(pos, output, sep=":")
+    return output
+
+
+
 
 # def create_wav(file_name):
 #     create_raw(file_name)
--- src/square/plot.py
+++ src/square/plot.py
@@ -5,26 +5,40 @@ import matplotlib.pyplot as plt
 from liner import create_raw, encode, decode
 
 
-file_name = "fb536381.txt"
+file_name = "fb536381.zip"
 
 if os.path.exists(f"{file_name}.result.json"):
     with open(f"{file_name}.result.json", mode="r") as f:
         result = json.load(f)
-        decode(result)
+        raw = result[-1][0]
+        for r in range(len(result) - 1, -1, -1):
+            moves = result[r][1]
+            raw = decode(raw, moves)
+        assert raw == create_raw(file_name)
+
+        moves = result[-1][0]
+        print(moves)
+        for r in range(len(result) - 1, -1, -1):
+            raw = result[r][1]
+            moves = decode(moves, raw)
+            break
+        print(raw)
+        print(moves)
 else:
     result = []
     count = 0
-    moves = create_raw(file_name)
-    first = moves[:]
+    raw = create_raw(file_name)
+    first = raw[:]
+    print(first)
     while True:
-        how, moves, raw = encode(moves)
+        raw, moves = encode(raw)
         # plt.plot(raw)
         # plt.show()
         count += 1
         print(count)
-        result.append([how, moves, raw])
-        # break
+        result.append([raw, moves])
         if max(moves) == 1 and max(raw) == 1:
+        # if moves == raw:
             break
     json_string = json.dumps(result)
     with open(f"{file_name}.result.json", mode="w") as f:
